bfa4de0e6b39459fbea043589c5603cba414b10e
pureJy <1041207253@qq.com>
2022-02-26 22:03:41 +0800

    dayone

---
+++ problems/剑指Offer-06.从尾到头打印链表.ts
@@ -0,0 +1,24 @@
+/**
+ * @link https://leetcode-cn.com/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/
+ */
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+ function reversePrint(head: ListNode | null): number[] {
+    const queue: Array<number> = [];
+    while(head !== null) {
+        queue.unshift(head.val);
+        head = head.next;
+    }
+    return queue;
+};
